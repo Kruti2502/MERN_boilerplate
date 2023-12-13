6285a9b61b55695f0c522c16c43b6d76e6f4e241
Kruti Shah <kruti.shah1625@gmail.com>
2023-12-13 17:03:16 +0530

    Changed default port

--- server/config.ts
+++ server/config.ts
@@ -8,7 +8,7 @@ dotenv.config({
 const config = {
   NODE_ENV: process.env.NODE_ENV || "development",
   HOST: process.env.HOST || "localhost",
-  PORT: +process.env.PORT! || 3000,
+  PORT: +process.env.PORT! || 3001,
   MONGO_URL: process.env.MONGO_URL,
 };
 
